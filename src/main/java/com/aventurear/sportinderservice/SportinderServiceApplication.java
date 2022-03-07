e7043083eb82c16ea83148339c807dc2fc735cfb
Pedro <russopedro87@gmail.com>
2022-03-07 16:50:53 -0300

    First commit

---
+++ src/main/java/com/aventurear/sportinderservice/SportinderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.aventurear.sportinderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportinderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportinderServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aventurear/sportinderservice/SportinderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aventurear.sportinderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportinderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
